8cc3de989b3a01cf42b612d6ed287832da1fc237
MaxBePunkt <maximillianborret@gmail.com>
2021-12-03 22:08:46 +0100

    1.4

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-let button = document.getElementsByTagName("button");
 let inputs = document.getElementsByTagName("input");
 let wrapDivRadios = document.querySelector(".wrapDivRadios");
 let resultPoints = document.querySelector("#resultPoints");
@@ -6,6 +5,8 @@ let mainSec = document.querySelector("#mainSec");
 let headerSec = document.querySelector("#headerSec");
 let playHead = document.querySelector("#playHead");
 let endHead = document.querySelector("#endHead");
+let user = document.querySelector(".user");
+let comp = document.querySelector(".comp");
 let compText = "";
 let count = 1;
 let rounds = 5;
@@ -14,7 +15,6 @@ let compPoints = 0;
 let rock = "rock";
 let paper = "paper";
 let scissors = "scissors";
-
 let radioChange = () => {
   for (let j = 0; j < inputs.length; j++) {
     if (inputs[j].checked) {
@@ -22,13 +22,11 @@ let radioChange = () => {
     }
   }
 };
-
 let startGame = (userChoice) => {
   // maximal rundenzahl
   if (count > rounds) {
     return null;
   }
-
   wrapDivRadios.innerHTML = `${count} / ${rounds}`;
   let compNum = Math.floor(Math.random() * 3);
 
@@ -52,6 +50,9 @@ let startGame = (userChoice) => {
       playHead.innerHTML = `${userChoice} beats ${compText}`;
       mainSec.classList.add("winner");
       mainSec.classList.remove("loser");
+      user.style.color = "rgba(24, 232, 72, 0.5)";
+      comp.style.color = "rgba(232, 24, 24, 0.5)";
+
       break;
     case "rockpaper":
       compPoints += 1;
@@ -59,7 +60,8 @@ let startGame = (userChoice) => {
       playHead.innerHTML = `${compText} beats ${userChoice}`;
       mainSec.classList.remove("winner");
       mainSec.classList.add("loser");
-
+      user.style.color = "rgba(232, 24, 24, 0.5)";
+      comp.style.color = "rgba(24, 232, 72, 0.5)";
       break;
     case "paperscissors":
       compPoints += 1;
@@ -67,7 +69,8 @@ let startGame = (userChoice) => {
       playHead.innerHTML = `${compText} beats ${userChoice}`;
       mainSec.classList.remove("winner");
       mainSec.classList.add("loser");
-
+      user.style.color = "rgba(232, 24, 24, 0.5)";
+      comp.style.color = "rgba(24, 232, 72, 0.5)";
       break;
     case "scissorspaper":
       userPoints += 1;
@@ -75,6 +78,8 @@ let startGame = (userChoice) => {
       playHead.innerHTML = `${userChoice} beats ${compText}`;
       mainSec.classList.add("winner");
       mainSec.classList.remove("loser");
+      user.style.color = "rgba(24, 232, 72, 0.5)";
+      comp.style.color = "rgba(232, 24, 24, 0.5)";
       break;
     case "scissorsrock":
       compPoints += 1;
@@ -82,6 +87,8 @@ let startGame = (userChoice) => {
       playHead.innerHTML = `${compText} beats ${userChoice}`;
       mainSec.classList.remove("winner");
       mainSec.classList.add("loser");
+      user.style.color = "rgba(232, 24, 24, 0.5)";
+      comp.style.color = "rgba(24, 232, 72, 0.5)";
       break;
     case "rockscissors":
       userPoints += 1;
@@ -89,21 +96,37 @@ let startGame = (userChoice) => {
       playHead.innerHTML = `${userChoice} beats ${compText}`;
       mainSec.classList.add("winner");
       mainSec.classList.remove("loser");
+      user.style.color = "rgba(24, 232, 72, 0.5)";
+      comp.style.color = "rgba(232, 24, 24, 0.5)";
       break;
     default:
       resultPoints.innerHTML = `${userPoints} : ${compPoints}`;
       playHead.innerHTML = `${userChoice} vs ${compText} : tie`;
       mainSec.classList.remove("winner");
       mainSec.classList.remove("loser");
+      user.style.color = "rgb(113, 113, 113)";
+      comp.style.color = "rgb(113, 113, 113)";
   }
 
   if (count >= rounds) {
     if (userPoints < compPoints) {
       endHead.innerHTML = `Defeat`;
+      mainSec.classList.remove("winner");
+      mainSec.classList.add("loser");
+      user.style.color = "rgba(232, 24, 24, 0.5)";
+      comp.style.color = "rgba(24, 232, 72, 0.5)";
     } else if (userPoints > compPoints) {
       endHead.innerHTML = `Winner`;
+      mainSec.classList.add("winner");
+      mainSec.classList.remove("loser");
+      user.style.color = "rgba(24, 232, 72, 0.5)";
+      comp.style.color = "rgba(232, 24, 24, 0.5)";
     } else if (userPoints == compPoints) {
       endHead.innerHTML = `Tie`;
+      mainSec.classList.remove("winner");
+      mainSec.classList.remove("loser");
+      user.style.color = "rgb(113, 113, 113)";
+      comp.style.color = "rgb(113, 113, 113)";
     }
   }
   count++;
